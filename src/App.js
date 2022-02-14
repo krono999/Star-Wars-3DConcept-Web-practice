8b54a0ef1f9ae8936ee93776aff033b7e94833d4
Nicolas Nuñez <72771920+krono999@users.noreply.github.com>
2022-02-14 03:21:58 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,55 +11,9 @@ import LoadingComponent from '../../Zenvia/src/components/Loading/Loading'
 // import AudioPlayers from '../../Zenvia/src/components/AudioPlayer/AudioPlayer'
 import SideNav from '../src/components/SideNav/SideNav'
 
-// let endpoints = [
-//   'https://swapi.dev/api/people/',
-//   'https://api.github.com/users/https://swapi.dev/api/people/?page=2',
-//   'https://api.github.com/users/https://swapi.dev/api/people/?page=3',
-//   'https://api.github.com/users/https://swapi.dev/api/people/?page=4',
-//   'https://api.github.com/users/https://swapi.dev/api/people/?page=5',
-//   'https://api.github.com/users/https://swapi.dev/api/people/?page=6',
-//   'https://api.github.com/users/https://swapi.dev/api/people/?page=7',
-//   'https://api.github.com/users/https://swapi.dev/api/people/?page=8',
-//   'https://api.github.com/users/https://swapi.dev/api/people/?page=9',
-
-// ];
-
 function App() {
   // const [characters, setCharacters] = useState([] || [{}])
   const [movies, setMovies] = useState([])
-
-
-  // let url1 = axios.get('https://swapi.dev/api/people/?page=1')
-  // let url2 = axios.get('https://swapi.dev/api/people/?page=2')
-  // let url3 = axios.get('https://swapi.dev/api/people/?page=3')
-
-
-  // async function getCharacters() {
-  //   let data = Promise.all([url1, url2, url3]).then((values) => {
-  //     console.log(values)
-  //   })
-
-  //   // for (let index = 1; index < 82; index++) {
-  //   //   try {
-  //   //     const res =
-  //   //       await axios.get(`https://swapi.dev/api/people/${index === 17 ? 18 :
-  //   //         index}`)
-  //   //     data.push(res.data)
-  //   //   } catch (error) {
-  //   //     console.log(error)
-  //   //   }
-
-  //   // }
-  //   console.log(data)
-
-  //   console.log('ADASDASDASD', data)
-
-  //   if (data) {
-  //     setCharacters(data)
-  //   }
-  // }
-
-
   const [characters, setCharacters] = useState([])
   let array = [];
   const endP = [
